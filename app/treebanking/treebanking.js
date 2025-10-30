96c0fb0032be03aed202476fc63359dde8210aae
sdubois2 <111801641+sdubois2@users.noreply.github.com>
2025-10-30 13:24:15 -0500

    Added functions for save state, undo, and redo.

--- app/treebanking/treebanking.js
+++ app/treebanking/treebanking.js
@@ -667,3 +667,75 @@ document.addEventListener('DOMContentLoaded', async () => {
   setupSentenceSelector();   // populate dropdown and link it
   setupResizeHandle();       // enable interactive resizing
 });
+
+
+/*
+*
+* The following code block implements undo and redo functionality
+* for the treebanking application. It maintains two stacks to track
+* changes to the tree structure, allowing users to revert or reapply
+* modifications as needed. In order for the stacks to be properly mantained,
+* there needs to be calls to saveState() function whenever a change is made to the tree.
+*
+* Samuel DuBois 2025 10 28
+*/
+let undoStack = [];
+let redoStack = [];
+let treeState = getInitialTree();
+
+// This function will copy the curret state of the tree.
+function saveState() {
+  undoStack.push(structuredClone(treeState));
+  redoStack = [];
+}
+
+// This function will revert the tree to its previous state.
+function undoButton(){
+  if (undoStack.length === 0) return;
+
+  redoStack.push(structuredClone(treeState));
+  treeState = undoStack.pop();
+  renderTree(treeState);
+  return;
+}
+
+// This is the event for the undo button.
+document.addEventListener("DOMContentLoaded", () => {
+  // Specify the button you are going to listen for
+  const button = document.getElementById("undo");
+
+  // Add the event listener to the button you are listening for.
+  button.addEventListener("click", async () => {
+    undoButton();
+  });
+});
+
+// This function will revert the tree to its previous change.
+function redoButton(){
+  if (redoStack.length === 0) return;
+
+  undoStack.push(structuredClone(treeState));
+  treeState = redoStack.pop();
+  renderTree(treeState);
+}
+
+// This is the event for the redo button.
+document.addEventListener("DOMContentLoaded", () => {
+  // Specify the button you are going to listen for
+  const button = document.getElementById("redo");
+
+  // Add the event listener to the button you are listening for.
+  button.addEventListener("click", async () => {
+    alert("Oops! This functionality is still under construction. Please check back soon!");
+    redoButton()
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("save");
+
+  button.addEventListener("click", async () => {
+    alert("Oops! This functionality is still under construction. Please check back soon!");
+  });
+});
